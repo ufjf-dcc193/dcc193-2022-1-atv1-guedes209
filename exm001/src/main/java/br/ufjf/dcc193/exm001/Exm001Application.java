ad87c2f8c8a7cfbbc7df6371b30bbb4c1f0c0150
ICE <ice@lab04m11.nrc.ice.ufjf.br>
2022-04-26 21:26:23 -0300

    hello

---
+++ exm001/src/main/java/br/ufjf/dcc193/exm001/Exm001Application.java
@@ -0,0 +1,14 @@
+package br.ufjf.dcc193.exm001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exm001Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Exm001Application.class, args);
+		System.out.println("Hello");
+	}
+
+}
---
+++ exm001/src/test/java/br/ufjf/dcc193/exm001/Exm001ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufjf.dcc193.exm001;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exm001ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
