16d522f8893298756e7e42c84d0b7569b8505fd2
guedes209 <bruno.guedes@ice.ufjf.br>
2022-04-26 22:14:13 -0300

    adicionado o cavaleiro no set

--- quest/src/main/java/br/ufjf/dcc193/bruno/quest/QuestApplication.java
+++ quest/src/main/java/br/ufjf/dcc193/bruno/quest/QuestApplication.java
@@ -9,9 +9,10 @@ public class QuestApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(QuestApplication.class, args);
 		System.out.println("Aventura");
-
+		Cavaleiro c1 = new Cavaleiro();
 		Missao m1 = new Missao();
 		m1.iniciar();
+		m1.setCavaleiro(c1);
 		m1.concluir();
 	}
 
