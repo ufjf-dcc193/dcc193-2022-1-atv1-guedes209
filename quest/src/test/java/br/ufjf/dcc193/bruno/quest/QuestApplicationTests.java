8cae1a4d48354dc2d090a3e6ddf80ae29434cd33
ICE <ice@lab04m11.nrc.ice.ufjf.br>
2022-04-26 21:31:26 -0300

    teste

---
+++ quest/src/test/java/br/ufjf/dcc193/bruno/quest/QuestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufjf.dcc193.bruno.quest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
