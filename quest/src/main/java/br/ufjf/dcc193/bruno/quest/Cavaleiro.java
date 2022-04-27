f504d4c36f698ab68a812036e8359eafbe0d4068
guedes209 <bruno.guedes@ice.ufjf.br>
2022-04-26 21:55:55 -0300

    Adicao da classe Cavaleiro e missao

---
+++ quest/src/main/java/br/ufjf/dcc193/bruno/quest/Cavaleiro.java
@@ -0,0 +1,12 @@
+package br.ufjf.dcc193.bruno.quest;
+
+public class Cavaleiro {
+
+    public Cavaleiro(){
+        System.out.println("O cavaleiro foi chamado");
+    }
+
+    public void agir(){
+        System.out.println("O Cavaleiro agiu");
+    }
+}
--- quest/src/main/java/br/ufjf/dcc193/bruno/quest/Missao.java
+++ quest/src/main/java/br/ufjf/dcc193/bruno/quest/Missao.java
@@ -2,8 +2,11 @@ package br.ufjf.dcc193.bruno.quest;
 
 public class Missao {
 
+    private Cavaleiro cavaleiro;
+
     public Missao(){
         System.out.println("A missao foi construida");
+        this.cavaleiro = new Cavaleiro();
     }
 
     public void iniciar(){
@@ -11,6 +14,7 @@ public class Missao {
     }
 
     public void concluir(){
+        cavaleiro.agir();
         System.out.println("Concluiu missao");
     }
 }
